89e6e8388d8a1ad04cf204c92c449b7d04cf0842
Sourabh Bansotiya <77042105+developer-sourabh28@users.noreply.github.com>
2024-05-20 16:46:01 +0000

    commit changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,20 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
-const count = () => {
-  const[count, setCount] = useState(1);
-  console.log(count);
+const App = () => {
+  const[count, setCount] = useState(0);
 
   const increment = () => {
     setCount(count+1);
   }
-const App = () => {
+
   return (
     <div>
         {/* Do not remove the main div */}
     <p>Button licked {count} times!</p>
    <button onClick={increment}>Click me</button>
     </div>
-
   )
 }
-}
+
 export default App
