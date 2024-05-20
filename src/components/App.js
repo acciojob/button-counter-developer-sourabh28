adaceab4c498569e178823c7be995c347d114389
Sourabh Bansotiya <77042105+developer-sourabh28@users.noreply.github.com>
2024-05-20 16:21:35 +0000

    solution added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const count = () => {
+  const[count, setCount] = useState(1);
+  console.log(count);
+
+  const increment = () => {
+    setCount(count+1);
+  }
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <p>Button licked {count} times!</p>
+   <button onClick={increment}>Click me</button>
     </div>
+
   )
 }
-
+}
 export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const root = ReactDom.createRoot(document.getElementById("root"));
 
+class Toggle extends Component{
+    constructor(props){
+        super(props);
+        this.state = {show: true};
+    }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+toggleApp = () => {
+    this.setState({show: !this.state.show});
+};
+
+    render(){
+        return(
+        <>
+        <button onClick={this.toggleApp}>Toggle App component</button>
+        {this.state.show && <App/>}
+        </>
+        )
+    }
+}
+root.render(<Toggle/>);
+
